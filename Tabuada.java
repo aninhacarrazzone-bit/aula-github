1caa2fa9cb5942b0a4018bc9cd57b909f62d3e76
Ana Andrade <aninhacarrazzone@gmail.com>
2026-04-18 11:39:22 -0300

    Incluir Tabuada

---
+++ Tabuada.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número:");
+
+        int numero = sc.nextInt();
+
+        sc.close();
+
+        if(numero < 0){
+           System.out.println("Número Inválido"); 
+           return;
+        }
+
+        for (int i = 1; i <= 10; i++) {
+             System.out.printf("%d X %d= %d\n", numero, i, numero*i);
+        }
+    }
+    
+}
