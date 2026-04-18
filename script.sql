ab18ddce00bbbb5b82ca707524af25ab8b58b612
Ana Andrade <aninhacarrazzone@gmail.com>
2026-04-18 13:23:58 -0300

    Inserção de registro na tabela

--- script.sql
+++ script.sql
@@ -1,8 +1,10 @@
 create database  treina_recife;
 
-create table tb_aalunos(
+create table tb_alunos(
     id Integer auto_increment primary key,
     nome varchar(50) not null,
     email varchar(150) not null,
     data_nascimento datetime not null
 )
+
+insert into tb_alunos values(1,'Ana Maria De Andrade', 'aninhacarrazzone@gmail.com', '1998-07-24');
